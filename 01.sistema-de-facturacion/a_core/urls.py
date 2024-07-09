a54a9489524e99842d33b25617403f9196ae2ae7
César P <cesarlpb@gmail.com>
2024-07-09 16:41:47 +0200

    Creamos template de invoices

--- 01.sistema-de-facturacion/a_core/urls.py
+++ 01.sistema-de-facturacion/a_core/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', home_view, name="home"),
     path('profile/', include('a_users.urls')),
     path('@<username>/', profile_view, name="profile"),
+    path('', include('a_invoices.urls')),
 ]
 
 # Only used when DEBUG=True, whitenoise can serve files when DEBUG=False
--- 01.sistema-de-facturacion/a_invoices/views.py
+++ 01.sistema-de-facturacion/a_invoices/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  print("Han solicitado invoices/")
+  return render(request, "a_invoices/index.html")
