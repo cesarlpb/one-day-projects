f58916985cd6deb49bf32c2efd95bed7fe052e9a
César P <cesarlpb@gmail.com>
2024-07-09 17:44:38 +0200

    Sacamos listas desde backend

--- 01.sistema-de-facturacion/a_invoices/views.py
+++ 01.sistema-de-facturacion/a_invoices/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 def index(request):
   print("Han solicitado invoices/")
-  return render(request, "a_invoices/index.html")
+  clientes = [1, 2, 3]
+  facturas = ['a', 'b', 'c']
+
+  context = {
+    "secret": "Supersafe123/",
+    "clientes": clientes,
+    "facturas": facturas,
+  }
+
+  return render(request, "a_invoices/index.html", context=context)
