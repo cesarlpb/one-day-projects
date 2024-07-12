506d0170977f20cb1f8c5a87e90c96d97762abd8
César P <cesarlpb@gmail.com>
2024-07-12 19:12:12 +0200

    bucle de proyectos en index

--- 04.sistema-gestion-proyectos/a_home/views.py
+++ 04.sistema-gestion-proyectos/a_home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from a_home.models import Proyecto
 
 def home_view(request):
     
     context = {
-        
+    "proyectos": list(Proyecto.objects.all())    
     }
     return render(request, 'index.html', context=context)
 
