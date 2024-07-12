d9363f659387c770bbd2093134f06250c6248b30
César P <cesarlpb@gmail.com>
2024-07-12 17:58:39 +0200

    on_delete en FK de lider_equipo en modelo Proyecto

--- 04.sistema-gestion-proyectos/a_home/models.py
+++ 04.sistema-gestion-proyectos/a_home/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Proyecto(models.Model):
   nombre = models.CharField(max_length=100)
   descripcion_corta = models.CharField(max_length=255)
-  lider_equipo = models.ForeignKey(User, related_name="lider_equipo")
+  lider_equipo = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="lider_equipo")
   imagen = models.FileField(upload_to="imagenes")
   url = models.URLField(blank=True, null=True)
 
