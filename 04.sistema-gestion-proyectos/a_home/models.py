da24b2a250d2dbb038f43ca7c534fd2dcc0dd27b
César P <cesarlpb@gmail.com>
2024-07-12 17:56:26 +0200

    modelo Proyecto y esquema de otros modelos

--- 04.sistema-gestion-proyectos/a_home/models.py
+++ 04.sistema-gestion-proyectos/a_home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Proyecto(models.Model):
+  nombre = models.CharField(max_length=100)
+  descripcion_corta = models.CharField(max_length=255)
+  lider_equipo = models.ForeignKey(User, related_name="lider_equipo")
+  imagen = models.FileField(upload_to="imagenes")
+  url = models.URLField(blank=True, null=True)
+
+
+class Desarrollador(models.Model):
+  pass
+
+class Repositorio(models.Model):
+  pass
+
+class Servidor(models.Model):
+  pass
+
+class Clave(models.Model):
+  pass
